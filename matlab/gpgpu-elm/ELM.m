68802dabc0bb6f1eda508e43674a2b732d2ddf77
Anderson Nascimento Nunes <andersonnnunes@hotmail.com.br>
2015-11-28 04:56:19 -0300

    sintetic_bench does not draw on the screen anymore.

--- matlab/gpgpu-elm/ELM.m
+++ matlab/gpgpu-elm/ELM.m
@@ -30,11 +30,11 @@ function [TrainingTime, TestingTime, TrainingAccuracy, TestingAccuracy] = elm(Tr
 % Sample1 regression: [TrainingTime, TestingTime, TrainingAccuracy, TestingAccuracy] = elm('sinc_train', 'sinc_test', 0, 20, 'sig')
 % Sample2 classification: elm('diabetes_train', 'diabetes_test', 1, 20, 'sig')
 %
-    %%%%    Authors:    MR QIN-YU ZHU AND DR GUANG-BIN HUANG
-    %%%%    NANYANG TECHNOLOGICAL UNIVERSITY, SINGAPORE
-    %%%%    EMAIL:      EGBHUANG@NTU.EDU.SG; GBHUANG@IEEE.ORG
-    %%%%    WEBSITE:    http://www.ntu.edu.sg/eee/icis/cv/egbhuang.htm
-    %%%%    DATE:       APRIL 2004
+	%%%%    Authors:    MR QIN-YU ZHU AND DR GUANG-BIN HUANG
+	%%%%    NANYANG TECHNOLOGICAL UNIVERSITY, SINGAPORE
+	%%%%    EMAIL:      EGBHUANG@NTU.EDU.SG; GBHUANG@IEEE.ORG
+	%%%%    WEBSITE:    http://www.ntu.edu.sg/eee/icis/cv/egbhuang.htm
+	%%%%    DATE:       APRIL 2004
 
 %%%%%%%%%%% Macro definition
 REGRESSION=0;
@@ -58,47 +58,48 @@ NumberofTestingData=size(TV.P,2);
 NumberofInputNeurons=size(P,1);
 
 if Elm_Type~=REGRESSION
-    %%%%%%%%%%%% Preprocessing the data of classification
-    sorted_target=sort(cat(2,T,TV.T),2);
-    label=zeros(1,1);                               %   Find and save in 'label' class label from training and testing data sets
-    label(1,1)=sorted_target(1,1);
-    j=1;
-    for i = 2:(NumberofTrainingData+NumberofTestingData)
-        if sorted_target(1,i) ~= label(1,j)
-            j=j+1;
-            label(1,j) = sorted_target(1,i);
-        end
-    end
-    number_class=j;
-    NumberofOutputNeurons=number_class;
-       
-    %%%%%%%%%% Processing the targets of training
-    temp_T=zeros(NumberofOutputNeurons, NumberofTrainingData);
-    for i = 1:NumberofTrainingData
-        for j = 1:number_class
-            if label(1,j) == T(1,i)
-                break; 
-            end
-        end
-        temp_T(j,i)=1;
-    end
-    T=temp_T*2-1;
+	%%%%%%%%%%%% Preprocessing the data of classification
+	sorted_target=sort(cat(2,T,TV.T),2);
+	label=zeros(1,1);                               %   Find and save in 'label' class label from training and testing data sets
+	label(1,1)=sorted_target(1,1);
+	j=1;
+	for i = 2:(NumberofTrainingData+NumberofTestingData)
+		if sorted_target(1,i) ~= label(1,j)
+			j=j+1;
+			label(1,j) = sorted_target(1,i);
+		end
+	end
+	number_class=j;
+	NumberofOutputNeurons=number_class;
+	   
+	%%%%%%%%%% Processing the targets of training
+	temp_T=zeros(NumberofOutputNeurons, NumberofTrainingData);
+	for i = 1:NumberofTrainingData
+		for j = 1:number_class
+			if label(1,j) == T(1,i)
+				break; 
+			end
+		end
+		temp_T(j,i)=1;
+	end
+	T=temp_T*2-1;
 
-    %%%%%%%%%% Processing the targets of testing
-    temp_TV_T=zeros(NumberofOutputNeurons, NumberofTestingData);
-    for i = 1:NumberofTestingData
-        for j = 1:number_class
-            if label(1,j) == TV.T(1,i)
-                break; 
-            end
-        end
-        temp_TV_T(j,i)=1;
-    end
-    TV.T=temp_TV_T*2-1;
+	%%%%%%%%%% Processing the targets of testing
+	temp_TV_T=zeros(NumberofOutputNeurons, NumberofTestingData);
+	for i = 1:NumberofTestingData
+		for j = 1:number_class
+			if label(1,j) == TV.T(1,i)
+				break; 
+			end
+		end
+		temp_TV_T(j,i)=1;
+	end
+	TV.T=temp_TV_T*2-1;
 
 end                                                 %   end if of Elm_Type
 
 %%%%%%%%%%% Move data to GPU
+gd = gpuDevice;
 T=gpuArray(T);
 P=gpuArray(P);
 TV.T=gpuArray(TV.T);
@@ -118,34 +119,35 @@ tempH=tempH+BiasMatrix;
 
 %%%%%%%%%%% Calculate hidden neuron output matrix H
 switch lower(ActivationFunction)
-    case {'sig','sigmoid'}
-        %%%%%%%% Sigmoid 
-        H = 1 ./ (1 + exp(-tempH));
-    case {'sin','sine'}
-        %%%%%%%% Sine
-        H = sin(tempH);    
-    case {'hardlim'}
-        %%%%%%%% Hard Limit
-        H = double(hardlim(tempH));
-    case {'tribas'}
-        %%%%%%%% Triangular basis function
-        H = tribas(tempH);
-    case {'radbas'}
-        %%%%%%%% Radial basis function
-        H = radbas(tempH);
+	case {'sig','sigmoid'}
+		%%%%%%%% Sigmoid 
+		H = 1 ./ (1 + exp(-tempH));
+	case {'sin','sine'}
+		%%%%%%%% Sine
+		H = sin(tempH);    
+	case {'hardlim'}
+		%%%%%%%% Hard Limit
+		H = double(hardlim(tempH));
+	case {'tribas'}
+		%%%%%%%% Triangular basis function
+		H = tribas(tempH);
+	case {'radbas'}
+		%%%%%%%% Radial basis function
+		H = radbas(tempH);
 	otherwise
-        error(['Unknown action: ' num2str(action)])
-        %%%%%%%% More activation functions can be added here                
+		error(['Unknown action: ' num2str(action)])
+		%%%%%%%% More activation functions can be added here                
 end
-clear tempH;                  %   Release the temparary array for calculation of hidden neuron output matrix H
+clear tempH;
 %%%%%%%%%%% Calculate output weights OutputWeight (beta_i)
 OutputWeight=pinv(pagefun(@ctranspose, H)) * pagefun(@ctranspose, T);
+wait(gd);
 TrainingTime=toc;
 
 %%%%%%%%%%% Calculate the training accuracy
 Y=pagefun(@ctranspose, (pagefun(@ctranspose, H) * OutputWeight));                             %   Y: the actual output of the training data
 if Elm_Type == REGRESSION
-    TrainingAccuracy=sqrt(mse(T - Y));              %   Calculate training accuracy (RMSE) for regression case
+	TrainingAccuracy=sqrt(mse(T - Y));              %   Calculate training accuracy (RMSE) for regression case
 end
 clear H;
 
@@ -157,26 +159,27 @@ ind=gpuArray.ones(1,NumberofTestingData);
 BiasMatrix=BiasofHiddenNeurons(:,ind);              %   Extend the bias matrix BiasofHiddenNeurons to match the demention of H
 tempH_test=tempH_test + BiasMatrix;
 switch lower(ActivationFunction)
-    case {'sig','sigmoid'}
-        %%%%%%%% Sigmoid 
-        H_test = 1 ./ (1 + exp(-tempH_test));
-    case {'sin','sine'}
-        %%%%%%%% Sine
-        H_test = sin(tempH_test);        
-    case {'hardlim'}
-        %%%%%%%% Hard Limit
-        H_test = hardlim(tempH_test);        
-    case {'tribas'}
-        %%%%%%%% Triangular basis function
-        H_test = tribas(tempH_test);        
-    case {'radbas'}
-        %%%%%%%% Radial basis function
-        H_test = radbas(tempH_test);        
+	case {'sig','sigmoid'}
+		%%%%%%%% Sigmoid 
+		H_test = 1 ./ (1 + exp(-tempH_test));
+	case {'sin','sine'}
+		%%%%%%%% Sine
+		H_test = sin(tempH_test);        
+	case {'hardlim'}
+		%%%%%%%% Hard Limit
+		H_test = hardlim(tempH_test);        
+	case {'tribas'}
+		%%%%%%%% Triangular basis function
+		H_test = tribas(tempH_test);        
+	case {'radbas'}
+		%%%%%%%% Radial basis function
+		H_test = radbas(tempH_test);        
 	otherwise
-        error(['Unknown action: ' num2str(action)])
-        %%%%%%%% More activation functions can be added here        
+		error(['Unknown action: ' num2str(action)])
+		%%%%%%%% More activation functions can be added here        
 end
 TY=pagefun(@ctranspose, (pagefun(@ctranspose, H_test) * OutputWeight));                       %   TY: the actual output of the testing data
+wait(gd);
 TestingTime=toc;
 %%%%%%%%%%% Gather data from GPU
 T=gather(T);
@@ -185,28 +188,28 @@ TV.T=gather(TV.T);
 TY=gather(TY);
 
 if Elm_Type == REGRESSION
-    TestingAccuracy=sqrt(mse(TV.T - TY));           %   Calculate testing accuracy (RMSE) for regression case
+	TestingAccuracy=sqrt(mse(TV.T - TY));           %   Calculate testing accuracy (RMSE) for regression case
 end
 
 if Elm_Type == CLASSIFIER
 %%%%%%%%%% Calculate training & testing classification accuracy
-    MissClassificationRate_Training=0;
-    MissClassificationRate_Testing=0;
+	MissClassificationRate_Training=0;
+	MissClassificationRate_Testing=0;
 
-    for i = 1 : size(T, 2)
-        [x, label_index_expected]=max(T(:,i));
-        [x, label_index_actual]=max(Y(:,i));
-        if label_index_actual~=label_index_expected
-            MissClassificationRate_Training=MissClassificationRate_Training+1;
-        end
-    end
-    TrainingAccuracy=1-MissClassificationRate_Training/size(T,2);
-    for i = 1 : size(TV.T, 2)
-        [x, label_index_expected]=max(TV.T(:,i));
-        [x, label_index_actual]=max(TY(:,i));
-        if label_index_actual~=label_index_expected
-            MissClassificationRate_Testing=MissClassificationRate_Testing+1;
-        end
-    end
-    TestingAccuracy=1-MissClassificationRate_Testing/size(TV.T,2);  
+	for i = 1 : size(T, 2)
+		[x, label_index_expected]=max(T(:,i));
+		[x, label_index_actual]=max(Y(:,i));
+		if label_index_actual~=label_index_expected
+			MissClassificationRate_Training=MissClassificationRate_Training+1;
+		end
+	end
+	TrainingAccuracy=1-MissClassificationRate_Training/size(T,2);
+	for i = 1 : size(TV.T, 2)
+		[x, label_index_expected]=max(TV.T(:,i));
+		[x, label_index_actual]=max(TY(:,i));
+		if label_index_actual~=label_index_expected
+			MissClassificationRate_Testing=MissClassificationRate_Testing+1;
+		end
+	end
+	TestingAccuracy=1-MissClassificationRate_Testing/size(TV.T,2);  
 end
--- matlab/sintetic/sintetic_bench.m
+++ matlab/sintetic/sintetic_bench.m
@@ -1,82 +1,72 @@
-%% Benchmarking pinv(A') * b' on the GPU
-
-% Copyright 2010-2013 The MathWorks, Inc.
-% Copyright 2015 Anderson Nascimento Nunes
+%{
+Benchmarking pinv(A') * b' on the GPU
+ Copyright 2010-2013 The MathWorks, Inc.
+ Copyright 2015 Anderson Nascimento Nunes 
+%}
 
 function results = sintetic_bench()
-%% The Benchmarking Function
-% We want to benchmark matrix left division (\), and not the cost of
+set(0,'DefaultFigureVisible','off');
+% The Benchmarking Function
+% We want to benchmark pinv(A') * b', and not the cost of
 % transferring data between the CPU and GPU, the time it takes to create a
 % matrix, or other parameters.  We therefore separate the data generation
 % from the solving of the linear system, and measure only the time it
 % takes to do the latter.
 function [A, b] = getData(n, clz)
-    fprintf('Creating a matrix of size %d-by-%d.\n', n, n/s_dim);
-    A = rand(n, n/s_dim, clz);
-    b = rand(n, n/s_dim, clz);
+	fprintf('Creating a matrix of size %d-by-%d.\n', n, n/s_dim);
+	A = rand(n, n/s_dim, clz);
+	b = rand(n, n/s_dim, clz);
 end
 
-function time = timeSolve(A, b, waitFcn)
-    tic;
-    x = pinv(A') * b'; %#ok<NASGU> We don't need the value of x.
-    waitFcn(); % Wait for operation to complete.
-    time = toc;
+function time = timeSolve(A, b, waitFcn, testCPU)
+	tic;
+	if testCPU==1
+		x = pinv(A') * b'; %#ok<NASGU> We don't need the value of x.
+	else
+		x = pinv(pagefun(@ctranspose, A)) * pagefun(@ctranspose, b); %#ok<NASGU> We don't need the value of x.
+	end
+	waitFcn(); % Wait for operation to complete.
+	time = toc;
 end
 
-%% Choosing Problem Size
+% Choosing Problem Size
 % As with a great number of other parallel algorithms, the performance of
 % solving a linear system in parallel depends greatly on the matrix size.
-% As seen in other examples, such as <paralleldemo_backslash_bench.html
-% Benchmarking A\b>, we compare the performance of the algorithm for
-% different matrix sizes.
+% We compare the performance of the algorithm for different matrix sizes.
 
 % Declare the matrix sizes to be a multiple of 1024.
-maxSizeSingle = 1024*20;
+maxSizeSingle = 1024*16;
 maxSizeDouble = maxSizeSingle/2;
 s_dim = 64;
 step = 1024;
 if maxSizeDouble/step >= 10
-    step = step*floor(maxSizeDouble/(5*step));
+	step = step*floor(maxSizeDouble/(5*step));
 end
 sizeSingle = 1024:step:maxSizeSingle;
 sizeDouble = 1024:step:maxSizeDouble;
 
 
-%% Comparing Performance: Gigaflops 
-% We use the number of floating point operations per second as our measure
-% of performance because that allows us to compare the performance of the
-% algorithm for different matrix sizes. 
-%
-% Given a matrix size, the benchmarking function creates the matrix |A| and
-% the right-hand side |b| once, and then solves |A\b| a few times to get
-% an accurate measure of the time it takes.  We use the floating point
-% operations count of the HPC Challenge, so that for an n-by-n matrix, we
-% count the floating point operations as |2/3*n^3 + 3/2*n^2|.
-%
-% The function is passed in a handle to a 'wait' function. On the CPU, this
-% function does nothing. On the GPU, this function waits for all pending
-% operations to complete. Waiting in this way ensures accurate timing.
-function time = benchFcn(A, b, waitFcn)
-    numReps = 3;
-    time = inf;
-    % We solve the linear system a few times and calculate the Gigaflops 
-    % based on the best time.
-    for itr = 1:numReps
-        tcurr = timeSolve(A, b, waitFcn);
-        time = min(tcurr, time);
-    end
-    
-    % Measure the overhead introduced by calling the wait function.
-    tover = inf;
-    for itr = 1:numReps
-        tic;
-        waitFcn();
-        tcurr = toc;
-        tover = min(tcurr, tover);
-    end
-    % Remove the overhead from the measured time. Don't allow the time to
-    % become negative.
-    time = max(time - tover, 0);
+% Comparing Performance: Time 
+function time = benchFcn(A, b, waitFcn, testCPU)
+	numReps = 3;
+	time = inf;
+	% We solve the linear system a few times and calculate the time spent
+	for itr = 1:numReps
+		tcurr = timeSolve(A, b, waitFcn, testCPU);
+		time = min(tcurr, time);
+	end
+	
+	% Measure the overhead introduced by calling the wait function.
+	tover = inf;
+	for itr = 1:numReps
+		tic;
+		waitFcn();
+		tcurr = toc;
+		tover = min(tcurr, tover);
+	end
+	% Remove the overhead from the measured time. Don't allow the time to
+	% become negative.
+	time = max(time - tover, 0);
 end
 
 % The CPU doesn't need to wait: this function handle is a placeholder.
@@ -86,10 +76,10 @@ end
 % On the GPU, to ensure accurate timing, we need to wait for the device
 % to finish all pending operations.
 function waitForGpu(theDevice)
-    wait(theDevice);
+	wait(theDevice);
 end
 
-%% Executing the Benchmarks
+% Executing the Benchmarks
 % Having done all the setup, it is straightforward to execute the
 % benchmarks.  However, the computations can take a long time to complete,
 % so we print some intermediate status information as we complete the
@@ -97,29 +87,30 @@ end
 % all the matrix sizes in a function, to benchmark both single- and 
 % double-precision computations.
 function [gflopsCPU, gflopsGPU] = executeBenchmarks(clz, sizes)
-    fprintf(['Starting benchmarks with %d different %s-precision ' ...
-         'matrices of sizes\nranging from %d-by-%d to %d-by-%d.\n'], ...
-            length(sizes), clz, sizes(1), sizes(1)/s_dim, sizes(end), ...
-            sizes(end)/s_dim);
-    gflopsGPU = zeros(size(sizes));
-    gflopsCPU = zeros(size(sizes));
-    gd = gpuDevice;
-    for i = 1:length(sizes)
-        n = sizes(i);
-        [A, b] = getData(n, clz);
-        gflopsCPU(i) = benchFcn(A, b, @waitForCpu);
-        fprintf('Time on CPU: %f\n', gflopsCPU(i));
-        A = gpuArray(A);
-        b = gpuArray(b);
-		fprintf('Free memory on GPU: %f\n', gd.FreeMemory);
-        gflopsGPU(i) = benchFcn(A, b, @() waitForGpu(gd));
-        fprintf('Time on GPU: %f\n', gflopsGPU(i));
-		clear('A','b');
-    end
+	fprintf(['Starting benchmarks with %d different %s-precision ' ...
+		 'matrices of sizes\nranging from %d-by-%d to %d-by-%d.\n'], ...
+			length(sizes), clz, sizes(1), sizes(1)/s_dim, sizes(end), ...
+			sizes(end)/s_dim);
+	gflopsGPU = zeros(size(sizes));
+	gflopsCPU = zeros(size(sizes));
+	gd = gpuDevice;
+	for i = 1:length(sizes)
+		n = sizes(i);
+		[A, b] = getData(n, clz);
+		gflopsCPU(i) = benchFcn(A, b, @waitForCpu, 1);
+		fprintf('Time on CPU: %f\n', gflopsCPU(i));
+		A = gpuArray(A);
+		b = gpuArray(b);
+		% fprintf('Free memory on GPU: %f\n', gd.FreeMemory);
+		gflopsGPU(i) = benchFcn(A, b, @() waitForGpu(gd), 2);
+		fprintf('Time on GPU: %f\n', gflopsGPU(i));
+		clear('A','b')
+	end
 end
 
-%%
+%
 % We then execute the benchmarks in single and double precision.
+close all;
 [cpu, gpu] = executeBenchmarks('single', sizeSingle);
 results.sizeSingle = sizeSingle;
 results.gflopsSingleCPU = cpu;
@@ -129,17 +120,16 @@ results.sizeDouble = sizeDouble;
 results.gflopsDoubleCPU = cpu;
 results.gflopsDoubleGPU = gpu;
 
-%% Plotting the Performance
+% Plotting the Performance
 % We can now plot the results, and compare the performance on the CPU and
 % the GPU, both for single and double precision.
 
-%%
-% First, we look at the performance of the backslash operator in single
+%
+% First, we look at the performance in single
 % precision.
 fig = figure;
 ax = axes('parent', fig);
-plot(ax, results.sizeSingle, results.gflopsSingleGPU, '-x', ...
-     results.sizeSingle, results.gflopsSingleCPU, '-o');
+plot(ax, results.sizeSingle, results.gflopsSingleGPU, '-x', results.sizeSingle, results.gflopsSingleCPU, '-o');
 grid on;
 legend('UPG', 'UCP', 'Location', 'NorthWest');
 title(ax, 'Desempenho com precisão simples.');
@@ -148,13 +138,12 @@ xlabel(ax, 'Tamanho da Dimensão Maior da Matriz');
 drawnow;
 print(fig, 'C:\Workspace\TCC_Text\sptime.png', '-dpng');
 
-%%
-% Now, we look at the performance of the backslash operator in double
-% precision.
+%
+% Now, we look at the performance in double precision.
 fig = figure;
 ax = axes('parent', fig);
 plot(ax, results.sizeDouble, results.gflopsDoubleGPU, '-x', ...
-     results.sizeDouble, results.gflopsDoubleCPU, '-o')
+	 results.sizeDouble, results.gflopsDoubleCPU, '-o')
 legend('UPG', 'UCP', 'Location', 'NorthWest');
 grid on;
 title(ax, 'Desempenho com precisão dupla.');
@@ -163,15 +152,14 @@ xlabel(ax, 'Tamanho da Dimensão Maior da Matriz');
 drawnow;
 print(fig, 'C:\Workspace\TCC_Text\dptime.png', '-dpng');
 
-%%
-% Finally, we look at the speedup of the backslash operator when comparing
-% the GPU to the CPU.
+%
+% Finally, we look at the speedup when comparing the GPU to the CPU.
 speedupDouble = results.gflopsDoubleCPU./results.gflopsDoubleGPU;
 speedupSingle = results.gflopsSingleCPU./results.gflopsSingleGPU;
 fig = figure;
 ax = axes('parent', fig);
 plot(ax, results.sizeSingle, speedupSingle, '-v', ...
-     results.sizeDouble, speedupDouble, '-*');
+	 results.sizeDouble, speedupDouble, '-*');
 grid on;
 legend('Precisão simples.', 'Precisão dupla.', 'Location', 'SouthEast');
 title(ax, 'Speedup da computação na UPG em comparação à UCP');
@@ -179,5 +167,4 @@ ylabel(ax, 'Speedup');
 xlabel(ax, 'Tamanho da Dimensão Maior da Matriz');
 drawnow;
 print(fig, 'C:\Workspace\TCC_Text\speedup.png', '-dpng');
-close all;
 end
