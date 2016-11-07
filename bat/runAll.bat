c393b4ebecac200db1f6fe144a7ad4b4755c94a9
Anderson Nascimento Nunes <andersonnnunes@hotmail.com.br>
2016-11-06 22:42:29 -0300

    Added 'human-readable detailed description' of the command.

--- bat/runAll.bat
+++ bat/runAll.bat
@@ -1,1 +1,7 @@
+:: Run all tests with the specified constraints.
+:: /HIGH : Process' priority is high.
+:: /affinity 80 : Set process' afinity to the designed core.
+:: -singleCompThread -noawt -minimize -nodesktop -nosplash : Set Matlab to run as a minimal shell.
+:: -r "run '"C:\Workspace\TCC_Code\matlab\runAll\runAll.m"'; exit;" : Execute the tests, then exit.
+
 C:\Windows\System32\cmd.exe /C start "" /HIGH /affinity 80 "C:\Program Files\MATLAB\R2015a\bin\win64\MATLAB.exe" -singleCompThread -noawt -minimize -nodesktop -nosplash -r "run '"C:\Workspace\TCC_Code\matlab\runAll\runAll.m"'; exit;"
